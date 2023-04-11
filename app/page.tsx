a68bce44b0b9e68eb05fc0a13e842d676e43f5ae
outdoteth <outdoteth@users.noreply.github.com>
2023-04-11 14:15:43 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,70 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <h1>NFT Finance Foundation</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <p>
+        NFT finance is a revolutionary ecosystem of financial tools and services
+        designed to unlock the full potential of the digital economy. From
+        lending markets to perpetual futures, options to insurance, NFT finance
+        provides a suite of powerful financial tools that have the potential to
+        transform the way we interact with digital objects.
+      </p>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <p>
+        Through this, NFT finance provides a gateway to a more vibrant and
+        accessible digital economy. By unlocking capital flows and streamlining
+        transactions, it democratizes access to everyone and provides
+        opportunities that were previously out of reach. Our vision is one where
+        digital objects are exchanged freely and efficiently, where the digital
+        economy is vibrant and thriving, and where anyone can interact with NFT
+        finance without barriers. The potential of NFT finance is limitless, and
+        we are committed to exploring every possibility to shape a better
+        future.
+      </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <p>
+        Despite the immense potential of NFT finance to transform the digital
+        economy, there are significant challenges that must be addressed in
+        order to fully realize its benefits. Many individuals lack financial
+        literacy and technical expertise, which makes it difficult for them to
+        effectively engage with NFT financial tooling. Additionally, there is a
+        lack of awareness about NFT finance and its potential, limiting its
+        adoption and growth. To overcome these challenges, we must take a
+        multi-faceted approach that includes education, awareness, and
+        connection-building.
+      </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+      <p>
+        We plan to create educational content and resources that will empower
+        individuals to better understand and engage with NFT finance. This will
+        include accessible information about how NFT finance works, its
+        potential benefits, and best practices for using NFT financial tools. In
+        addition to education, we will improve awareness of NFT finance by
+        supporting non-profit, unbiased research. We believe that this will help
+        to create a more comprehensive and objective understanding of NFT
+        finance, while also improving its reputation and legitimacy. Finally, we
+        recognize the need to improve connections between NFT finance
+        participants and sources of liquidity. By facilitating deal flow and
+        making it easier for users to access liquidity, we can help to create a
+        more vibrant and active NFT finance ecosystem.
+      </p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p>
+        We want NFT finance to be accessible to everyone, regardless of their
+        financial literacy or technical expertise. We believe that by empowering
+        individuals with the tools and knowledge they need to engage with NFT
+        finance, we can create a more equitable and sustainable digital economy.
+      </p>
+
+      <p>
+        In conclusion, we call upon all innovators and thinkers to join us in
+        our mission to unlock the power of NFT finance and usher in a new era of
+        innovation and progress in the digital economy. Together, we can shape a
+        brighter future.
+      </p>
     </main>
-  )
+  );
 }
